05a18e5e1065365c61a223f70507e99dd8a719c9
Anas Jawed <103234658+anasjawed283@users.noreply.github.com>
2024-03-14 05:07:58 +0530

    basic operation

---
+++ 2-Addition.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Define two variables to hold the numbers
+    num1 := 10
+    num2 := 20
+
+    // Add the two numbers
+    sum := num1 + num2
+
+    // Print the result
+    fmt.Printf("The sum of %d and %d is %d\n", num1, num2, sum)
+}
